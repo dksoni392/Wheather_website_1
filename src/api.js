859510b1e01f5890607ec7881838f1611cc64e91
Deepak Soni <144583639+dksoni392@users.noreply.github.com>
2024-08-16 01:00:45 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "83ad7a0de8msh0089760c2600d81p1b0b50jsn295ec0c53896",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "626af75c23721c7ed99f4204201e06ae"; // enter your key from openweather API
